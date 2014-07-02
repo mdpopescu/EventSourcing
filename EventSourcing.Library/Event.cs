ace90fc3834008b0df42cb252cf23240fce3a5c4
Marcel Popescu <mdpopescu@gmail.com>
2014-07-02 06:05:47 +0300

    initial commit

---
+++ EventSourcing.Library/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ProtoBuf;
+
+namespace EventSourcing.Library
+{
+  [ProtoContract(SkipConstructor = true)]
+  public class Event
+  {
+    public Guid Id
+    {
+      get { return id; }
+    }
+
+    public Event(Guid id)
+    {
+      this.id = id;
+    }
+
+    //
+
+    [ProtoMember(1)] private readonly Guid id;
+  }
+}
---
+++ EventSourcing.Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using EventSourcing.Library;
+using ProtoBuf;
+
+namespace EventSourcing.Server
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      using (var ms = new MemoryStream())
+      {
+        var guid = Guid.NewGuid();
+        var val = new Event(guid);
+        Serializer.Serialize(ms, val);
+        ms.Position = 0;
+        var clone = Serializer.Deserialize<Event>(ms);
+        Debug.Assert(guid == clone.Id);
+      }
+    }
+  }
+}
